5789c1716bee92b8efe3f4e368d8f09ca86dca6e
TahaBerk <121947272+Vubulub@users.noreply.github.com>
2024-06-07 02:57:58 +0300

    Update Project1_Test.py

--- Project1_Test.py
+++ Project1_Test.py
@@ -1,35 +1,38 @@
+# import necessary libraries
+import pickle
 import pyaudio
 import numpy as np
 import librosa
-from keras.models import model_from_json
-import pickle
 import argparse
 import cv2
+import os
+import random
+import threading
+import speech_recognition as sr
+from keras.models import model_from_json
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import os
-import random
-import speech_recognition as sr
-import threading
 
+# suppress TensorFlow warning messages
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# Load the pre-trained model and associated scaler and encoder for speech emotion recognition
-json_file = open('Models/speech_emotion_detection_model.json', 'r')
+# load the speech emotion detection model
+json_file = open('models/speech_emotion_detection_model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("Models/speech_emotion_detection_model.h5")
+loaded_model.load_weights("models/speech_emotion_detection_model.h5")
 
+# load the scaler
 with open('scaler.pickle', 'rb') as f:
     scaler = pickle.load(f)
 
-# Define mapping of indices to emotion labels for speech emotion recognition
+# define emotion labels and a dictionary
 emotion_labels = {0: 'neutral', 1: 'calm', 2: 'happy', 3: 'sad', 4: 'angry', 5: 'fearful', 6: 'disgust', 7: 'surprised'}
-emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+emotion_dict = {0: "angry", 1: "disgusted", 2: "fearful", 3: "happy", 4: "neutral", 5: "sad", 6: "surprised"}
 
-# Define function to extract features from audio data for speech emotion recognition
+# function to extract audio features
 def extract_features(data, sample_rate):
     zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
     stft = np.abs(librosa.stft(data))
@@ -39,7 +42,7 @@ def extract_features(data, sample_rate):
     mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
     return np.concatenate((zcr, chroma_stft, mfcc, rms, mel))
 
-# Define function to predict emotion from features for speech emotion recognition
+# function to predict emotion
 def predict_emotion(features):
     scaled_features = scaler.transform(features.reshape(1, -1))
     reshaped_features = np.expand_dims(scaled_features, axis=2)
@@ -48,7 +51,7 @@ def predict_emotion(features):
     emotion_label = emotion_labels[emotion_index]
     return emotion_label
 
-# Define parameters for audio recording
+# audio settings
 FORMAT = pyaudio.paFloat32
 CHANNELS = 1
 RATE = 22050
@@ -56,21 +59,20 @@ CHUNK = 1024
 SECONDS = 0.1
 THRESHOLD = 0.0001
 
-# Initialize PyAudio
 audio = pyaudio.PyAudio()
 
-# Open a stream to capture audio from microphone
+# open audio stream
 stream = audio.open(format=FORMAT, channels=CHANNELS,
                     rate=RATE, input=True,
                     frames_per_buffer=int(RATE * SECONDS))
 
 print("Listening for speech...")
 
-# Define a global variable to store the predicted emotion
 emotion = ""
-# Continuously record and process audio for speech emotion recognition
+
+# function to process audio in a separate thread
 def process_audio():
-    global emotion  # Declare emotion as a global variable
+    global emotion
     while True:
         data = stream.read(int(RATE * SECONDS))
         audio_data = np.frombuffer(data, dtype=np.float32)
@@ -83,12 +85,12 @@ audio_thread = threading.Thread(target=process_audio)
 audio_thread.daemon = True
 audio_thread.start()
 
-# command line argument
+# argument parser
 ap = argparse.ArgumentParser()
 ap.add_argument("--mode", help="display")
 mode = ap.parse_args().mode
 
-# Define data generators for facial emotion recognition
+# directories and parameters for image data generator
 train_dir = 'train'
 val_dir = 'test'
 
@@ -97,9 +99,11 @@ num_val = 7178
 batch_size = 64
 num_epoch = 50
 
+# image data generators
 train_datagen = ImageDataGenerator(rescale=1./255)
 val_datagen = ImageDataGenerator(rescale=1./255)
 
+# train generator
 train_generator = train_datagen.flow_from_directory(
         train_dir,
         target_size=(48, 48),
@@ -107,6 +111,7 @@ train_generator = train_datagen.flow_from_directory(
         color_mode="grayscale",
         class_mode='categorical')
 
+# validation generator
 validation_generator = val_datagen.flow_from_directory(
         val_dir,
         target_size=(48, 48),
@@ -114,9 +119,10 @@ validation_generator = val_datagen.flow_from_directory(
         color_mode="grayscale",
         class_mode='categorical')
 
-# Create the facial emotion recognition model
+# define the CNN model
 model = Sequential()
 
+# add convolutional layers to the model
 model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -128,26 +134,29 @@ model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
+# flatten the layers and add dense layers
 model.add(Flatten())
 model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(7, activation='softmax'))
 
+# load the model weights if in test mode
 if mode == "test":
-    model.load_weights('Models/face_emotion_model.h5')
+    model.load_weights('models/face_emotion_model.h5')
 
+    # initialize the webcam
     cv2.ocl.setUseOpenCL(False)
-
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print("Error: Couldn't open webcam")
         exit()
 
+    # initialize speech recognition
     recognizer = sr.Recognizer()
     mic = sr.Microphone()
-
     speech_text = ""
 
+    # function to recognize speech
     def recognize_speech():
         global speech_text
         while True:
@@ -162,12 +171,11 @@ if mode == "test":
             except sr.RequestError as e:
                 speech_text = f"Error: {e}"
 
-    # Start the speech recognition thread
+    # start speech recognition thread
     speech_thread = threading.Thread(target=recognize_speech)
     speech_thread.daemon = True
     speech_thread.start()
 
-    # Initialize emotion lists and indices
     speech_emotions = list(emotion_labels.values())
     face_emotions = list(emotion_dict.values())
     speech_target = random.choice(speech_emotions)
@@ -175,6 +183,7 @@ if mode == "test":
     speech_score = 0
     face_score = 0
 
+    # main loop for webcam processing
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -196,12 +205,10 @@ if mode == "test":
             cv2.putText(frame, text, (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
             face_emotion_detected = emotion_dict[maxindex]
 
-        # Check if the detected speech emotion matches the speech target
         if emotion == speech_target:
             speech_target = random.choice(speech_emotions)
             speech_score += 1
 
-        # Check if the detected face emotion matches the face target
         if face_emotion_detected == face_target:
             face_target = random.choice(face_emotions)
             face_score += 1
@@ -215,10 +222,8 @@ if mode == "test":
         cv2.putText(frame, f"Face Score: {face_score}", (470, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 255, 255), 2,
                     cv2.LINE_AA)
 
-        # Display the recognized speech emotion
         cv2.putText(frame, f"Speech Emotion: {emotion}", (10, frame.shape[0] - 60), cv2.FONT_HERSHEY_SIMPLEX, 0.55,
                     (0, 255, 0), 2, cv2.LINE_AA)
-        # Display the recognized speech at the bottom left corner
         cv2.putText(frame, speech_text, (10, frame.shape[0] - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
 
         cv2.imshow('Video', cv2.resize(frame, (1280, 720), interpolation=cv2.INTER_CUBIC))
